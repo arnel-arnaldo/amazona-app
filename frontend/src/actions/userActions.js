1aa5dbc9457996168a12890be5f006c08a66685f
Arnel <arnelarnaldo@gmail.com>
2021-05-20 15:54:03 -0600

    Part 33 - Display User Profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {USER_REGISTER_FAIL,
+import {USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_REGISTER_FAIL,
         USER_REGISTER_REQUEST,
         USER_REGISTER_SUCCESS,
         USER_SIGNIN_FAIL,
@@ -62,4 +62,23 @@ export const signout = () => (dispatch) => {
     localStorage.removeItem('shippingAddress');
     dispatch({ type: USER_SIGNIN_SIGNOUT });
     document.location.href = '/signin';
+};
+
+export const detailsUser = (userId) => async (dispatch, getState) => {
+    dispatch({ type: USER_DETAILS_REQUEST, payload: userId });
+    const { userSignin: { userInfo } } = getState();
+    try {
+        const { data } = await axios.get(`/api/users/${userId}`, {
+            headers: { Authorization: `Bearer ${userInfo.token}` }
+        });
+        dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload: 
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message
+        });
+    }
 };
