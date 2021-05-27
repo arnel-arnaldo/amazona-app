016e2e05b99760b0282cfb01c327613843f6f33f
Arnel <arnelarnaldo@gmail.com>
2021-05-27 10:04:09 -0600

    Part 46 - List Users

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import User from '../models/userModel.js';
-import { generateToken, isAuth } from '../utils.js';
+import { generateToken, isAdmin, isAuth } from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -74,6 +74,12 @@ userRouter.put('/profile', isAuth, expressAsyncHandler(async (req, res) => {
             token: generateToken(updatedUser)
         });
     }
+}));
+
+// Create API to list all users
+userRouter.get('/', isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
+    const users = await User.find({}); // get all users from database
+    res.send(users);
 }));
 
 export default userRouter;
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,7 +4,14 @@ import {USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_REGI
         USER_REGISTER_SUCCESS,
         USER_SIGNIN_FAIL,
         USER_SIGNIN_REQUEST,
-        USER_SIGNIN_SIGNOUT,USER_SIGNIN_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS
+        USER_SIGNIN_SIGNOUT,
+        USER_SIGNIN_SUCCESS,
+        USER_UPDATE_PROFILE_FAIL,
+        USER_UPDATE_PROFILE_REQUEST,
+        USER_UPDATE_PROFILE_SUCCESS,
+        USER_LIST_REQUEST,
+        USER_LIST_SUCCESS,
+        USER_LIST_FAIL
         } from "../constants/userConstants";
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -102,6 +109,27 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
                 error.response && error.response.data.message
                     ? error.response.data.message
                     : error.message
+        });
+    }
+};
+
+export const listUsers = () => async (dispatch, getState) => {
+    dispatch({ type: USER_LIST_REQUEST });
+    const { userSignin: { userInfo} } = getState();
+    try {
+        const { data } = await axios.get('/api/users', {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        });
+        dispatch({ type: USER_LIST_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: USER_LIST_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
         });
     }
 };
