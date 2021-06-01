a3ba49b4db4a552dac4b93ed8577e441b89b6cf6
Arnel <arnelarnaldo@gmail.com>
2021-06-01 11:44:39 -0600

    Part 55 - Complete Advanced Search

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -18,12 +18,20 @@ import { PRODUCT_CREATE_FAIL,
          PRODUCT_CATEGORY_LIST_SUCCESS,
          PRODUCT_CATEGORY_LIST_FAIL} from "../constants/productConstants";
 
-export const listProducts = ({ seller = '', name = '', category = '' }) => async (dispatch) => {
+export const listProducts = ({ seller = '',
+                                name = '',
+                                category = '',
+                                order = '',
+                                min = 0,
+                                max = 0,
+                                rating = 0
+                            }) => async (dispatch) => {
     dispatch({
         type: PRODUCT_LIST_REQUEST,
     });
     try {
-        const { data } = await axios.get(`/api/products?seller=${seller}&name=${name}&category=${category}`);
+        const { data } = await axios.get(
+            `/api/products?seller=${seller}&name=${name}&category=${category}&min=${min}&max=${max}&rating=${rating}&order=${order}`);
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });        
     } catch (error) {
         dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message});
